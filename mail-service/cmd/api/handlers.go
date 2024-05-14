aad78e75e50c3d40a35e470110751b126f17d5a1
oxyprogrammer <oxyprogrammer@gmail.com>
2024-05-14 11:33:26 +0000

    Progressed with route.

--- mail-service/cmd/api/handlers.go
+++ mail-service/cmd/api/handlers.go
@@ -3,19 +3,27 @@ package main
 import "net/http"
 
 func (app *Config) SendMail(w http.ResponseWriter, r *http.Request) {
-	// var requestPayload RequestPayload
-	// err := app.readJSON(w, r, &requestPayload)
-	// if err != nil {
-	// 	app.errorJSON(w, err)
-	// 	return
-	// }
 
-	// switch requestPayload.Action {
-	// case "auth":
-	// 	app.authenticate(w, requestPayload.Auth)
-	// case "log":
-	// 	app.logItem(w, requestPayload.Log)
-	// default:
-	// 	app.errorJSON(w, errors.New("unknown action"))
-	// }
+	type mailMessage struct {
+		From    string `json:"from"`
+		To      string `json:"to"`
+		Subject string `json:"subject"`
+		Message string `json:"message"`
+	}
+	var requestPayload mailMessage
+	err := app.readJSON(w, r, &requestPayload)
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	msg := Message{
+		From:    requestPayload.From,
+		To:      requestPayload.To,
+		Subject: requestPayload.Subject,
+		Data:    requestPayload.Message,
+	}
+
+	// err := app.Mailer.SendSMTPMessage(msg)
 }
