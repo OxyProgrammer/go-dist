219ca1d53391d0dff8fe4ea89aceae531e75d859
oxyprogrammer <oxyprogrammer@gmail.com>
2024-05-14 09:02:13 +0000

    Mailer service implementation

---
+++ mail-service/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Config struct {
+}
+
+const webPort = "80"
+
+func main() {
+	app := Config{}
+
+	log.Println("Starting mail service on port", webPort)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	//Start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
