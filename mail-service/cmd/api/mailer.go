28ab94cb3cf5976a365c4867857d88020ed72a69
oxyprogrammer <oxyprogrammer@gmail.com>
2024-05-14 09:36:27 +0000

    Started implementing mailer

--- mail-service/cmd/api/mailer.go
+++ mail-service/cmd/api/mailer.go
@@ -1,1 +1,44 @@
 package main
+
+type Mail struct {
+	Domain      string
+	Host        string
+	Port        int
+	Username    string
+	Password    string
+	Encryption  string
+	FromAddress string
+	FromName    string
+}
+
+type Message struct {
+	From        string
+	FromName    string
+	To          string
+	Subject     string
+	Attachments []string
+	Data        interface{}
+	DataMap     map[string]any
+}
+
+func (m *Mail) SendSMTPMMessage(msg Message) error {
+	if msg.From == "" {
+		msg.From = m.FromAddress
+	}
+
+	if msg.FromName == "" {
+		msg.FromName = m.FromName
+	}
+
+	data := map[string]any{
+		"message": msg.Data,
+	}
+
+	msg.DataMap = data
+
+	formattedMessage, err := m.buildHTMLMessage(msg)
+}
+
+func (m *Mail) buildHTMLMessage(msg Message) (string, error) {
+	templateToRender := "./templates/mail.html.gohtml"
+}
